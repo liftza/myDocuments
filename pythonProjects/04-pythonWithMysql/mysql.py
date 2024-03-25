532d4fa46d1fbd43c471a6e9fe8e1dd0ad23fabf
liftza <164734726+liftza@users.noreply.github.com>
2024-03-25 12:30:51 +0700

    Add files via upload

---
+++ pythonProjects/04-pythonWithMysql/mysql.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+# Establishing a connection to the MySQL database
+conn = mysql.connector.connect(
+    host="localhost",
+    user="springstudent",
+    password="springstudent",
+    database="webscarpsample"
+)
+
